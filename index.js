a6c7450562fa7c979993933ddb11ad5c4f9f9956
pashakeeper <pashagkjunost@gmail.com>
2022-09-28 20:04:42 +0300

    pre finish

---
+++ index.js
@@ -0,0 +1,60 @@
+lottie.loadAnimation({
+    container: document.getElementById('dog'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/240x400.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('320x100'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/320x100.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('300x250'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/300x250.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('300x600'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/300x600.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('336x280'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/336x280.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('480x800'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/480x800.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('728x90'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/728x90.json' // the path to the animation json
+  });
+lottie.loadAnimation({
+    container: document.getElementById('970x250'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animated_banners_json/970x250.json' // the path to the animation json
+  });
+
+
+
+
